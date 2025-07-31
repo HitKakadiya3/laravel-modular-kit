de1132306dc801cebc62b3b5feaa1b9de7a7f91d
Hitendra Kakadiya <79523092+HitendraKakadiya@users.noreply.github.com>
2025-07-31 11:59:15 +0530

    add post API and UI also made it in modular format

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,3 @@
 
 require base_path('app/Modules/User/Routes/api.php');
 require base_path('app/Modules/Post/Routes/api.php');
-
-// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-//     return $request->user();
-// });
